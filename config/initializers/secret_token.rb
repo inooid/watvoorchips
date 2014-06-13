bab02c7561fecbcf6d11c42db6ee4966a5ee6e2f
Koop Otten <koop@noorderstorm.com>
2014-06-13 13:05:31 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watvoorchips::Application.config.secret_key_base = '622f2838a5b850c5e36e829c296a897bea35f61db3338f146d7c5660c870af8b64aa31c82b3f1fe48cd33de665f07f7d9734069256591add71f4c555bd8d2b67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Watvoorchips::Application.config.session_store :cookie_store, key: '_watvoorchips_session'
