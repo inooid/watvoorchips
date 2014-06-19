fc772d13b372c115af13e5eb77c416a591e44b15
Koop Otten <koop@noorderstorm.com>
2014-06-19 12:57:48 +0200

    new Images and add2home

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,47 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 Item.create([
   { name: "Paprika Normal",
     image_link: "paprika-normal.png"
     },
-
   { name: "Naturel Normal",
     image_link: "naturel-normal.png"
     },
-
+  { name: "Doritos Sweet Chili Peper",
+    image_link: "doritos-sweet-chili-peper.png"
+    },
+  { name: "Doritos Cool American",
+    image_link: "doritos-cool-american.png"
+    },
+  { name: "Bugles Nacho Cheese",
+    image_link: "bugles-nacho-cheese.png"
+    },
+  { name: "Cheese Onion",
+    image_link: "cheese-onion.png"
+    },
+  { name: "Nibb-it Rings",
+    image_link: "nibb-it-rings.png"
+    },
+  { name: "Patat Joppie",
+    image_link: "patat-joppie.png"
+    },
+  { name: "Paprika Wokkels",
+    image_link: "paprika-wokkels.png"
+    },
+  { name: "Naturel Wokkels",
+    image_link: "naturel-wokkels.png"
+    },
   { name: "Bolognese Normal",
     image_link: "naturel-normal.png"
     },
-
-  { name: "Naturel Normal",
-    image_link: "naturel-normal.png"
+  { name: "Barbecue Ham",
+    image_link: "barbecue-ham.png"
     },
-
-  { name: "Naturel Normal",
-    image_link: "naturel-normal.png"
+  { name: "Pringels Original",
+    image_link: "pringles-original.png"
     },
+  { name: "Pringels Paprika",
+    image_link: "pringles-paprika.png"
+    }
 ])
